07d2e280f336cc8faf51855fed657ebbded81741
Anirup Patnaik <anirup049@gmail.com>
2021-06-20 17:11:17 +0530

    Header component

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from "./Header.module.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 const Header = () => {
   return (
@@ -9,8 +11,9 @@ const Header = () => {
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
 
-      <div className={classes.header_search}>
+      <div className={classes.header__search}>
         <input className={classes.header__searchInput} type="text" />
+        <SearchIcon className={classes.header__searchIcon} />
       </div>
 
       <div className={classes.header__nav}>
@@ -27,6 +30,15 @@ const Header = () => {
         <div className={classes.header__options}>
           <span className={classes.header__optionsLineOne}>Your</span>
           <span className={classes.header__optionsLineTwo}>Prime</span>
+        </div>
+
+        <div className={classes.header__optionBasket}>
+          <ShoppingBasketIcon />
+          <span
+            className={`${classes.header__optionsLineTwo} ${classes.header__basketCount}`}
+          >
+            0
+          </span>
         </div>
       </div>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import classes from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={classes.home}>
+      <div className={classes.home__container}>
+        <img
+          alt=""
+          className={classes.home__image}
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Fashion/Event/Gateway/WRS-Jun/GW_graphics_Family/5._CB666661025_.jpg"
+        />
+      </div>
+
+      <div className={classes.home__row}>
+        {
+          // Product
+          // Product
+          // Product
+        }
+      </div>
+
+      <div className={classes.home__row}>
+        {
+          // Product
+          // Product
+        }
+      </div>
+
+      <div className={classes.home__row}>
+        {
+          // Product
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Home;
